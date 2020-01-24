f6def8011faf0630175f36e65b7ca608c6754465
Shabthi <shabthij@gmail.com>
2020-01-24 18:43:19 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include <string>
 #include <sstream>
 #include <cstdlib>
-#include <iostream>
 #include <fstream>
+#include <algorithm>
+#include<string.h>
 
 using namespace std;
 
@@ -31,12 +32,12 @@ std::string trim(const std::string& s)
 
 struct Flight
 {
-    std::string m_flightNumber;
-    std::string m_date;
-    std::string m_time;
-    std::string m_departVenue;
-    std::string m_arriveVenue;
-    std::string m_seats;
+    string m_flightNumber;
+    string m_date;
+    string m_time;
+    string m_departVenue;
+    string m_arriveVenue;
+    string m_seats;
 };
 
 class FlightDock
@@ -46,20 +47,134 @@ class FlightDock
         virtual ~FlightDock();
 
         unsigned int getFlightCount() { return m_counter; }
-        bool findFlight(const string& flightNumber, Flight* myFlight = NULL);
+        bool findFlight(const string& flightNumber);
+        bool findFlight(const string& flightNumber,int& myFlightIdx);
         void addFlight(string flightNumber, string data, string time, string departVenue, string arriveVenue, string seats);
         bool bookSeat(string flightNumber, string seat);
+        bool displayFlight(const string& flightNumber);
+        void getAll();
+        bool checkAvail(string flightNumber,int num);
+        void writeFile();
 
     private:
         unsigned int m_counter;
         struct Flight* m_flightArray;
         unsigned int m_maxFlightCounter;
 };
+bool FlightDock::checkAvail(string flightNumber,int num)
+{
+	
+//    Flight * myFlight;
+//    int myFlightIdx;
+//    int count = 0;
+//    int position=0;
+//    if(!findFlight(flightNumber, myFlightIdx))
+//    {
+//        cerr<<"invalid flight number"<<cout;
+//        return false;
+//    }
+//    myFlight = &m_flightArray[myFlightIdx];
+//    bool avail = false;
+//    
+//	string seatString = myFlight->m_seats;
+//     count = std::count(seatString.begin(), seatString.end(), '\n');
+//     count += 1 ;
+//     if (count >= num )
+//     {
+//     	avail = true;
+//	 }
+int count =0;
+Flight * myFlight;
+int myFlightIdx;
+findFlight(flightNumber, myFlightIdx);
+myFlight = &m_flightArray[myFlightIdx];
+bool avail = false;
+string seatString = myFlight->m_seats;
+stringstream iss(seatString);
+ while(iss.rdbuf()->in_avail() != 0)
+    {
+        int temRowNumber;
+        char tempSeatClass;
+        string tempSeatNumber;
+        stringstream buff;
+        try
+        {
+            if(iss.rdbuf()->in_avail() != 0)
+                iss >> temRowNumber;
+            else
+                break;
 
+            if(iss.rdbuf()->in_avail() != 0)
+                iss >> tempSeatClass;
+            else
+                break;
 
+            if(iss.rdbuf()->in_avail() != 0)
+			{
+				iss >> tempSeatNumber;
+                count += tempSeatNumber.length();
+			}
+                
+            else
+                break;
+        }
+        catch (std::exception& ex)
+        {
+            cerr<<ex.what()<<endl;
+            return false;
+        }
+    }
+	 if (count>=num)
+	 {
+	 	avail = true;
+	 }
+return avail;	
+}
+
+bool FlightDock::displayFlight(const string& flightNumber)
+{
+    int myFlightIdx;
+    if(!findFlight(flightNumber, myFlightIdx))
+    {
+        return false;
+    }
+    Flight *myFlight = &m_flightArray[myFlightIdx];
+    cout<<"FLIGHT_NAME: "<<myFlight->m_flightNumber<<endl;
+    cout<<"DATE_&_TIME: "<<myFlight->m_date<<" "<<myFlight->m_time<<endl;
+    cout<<"DEPARTURE: "<<myFlight->m_departVenue<<endl;
+    cout<<"ARRIVAL: "<<myFlight->m_arriveVenue<<endl;
+    cout<<"SEATS AVAILABLE"<<endl;
+    cout<<"---------------"<<endl;
+    cout<<myFlight->m_seats<<endl<<endl;
+    return true;
+}
+void FlightDock::writeFile()
+{
+	ofstream myfile;
+	myfile.open ("Flights1.txt");
+	if(myfile.is_open())
+	{
+		for( int i = 0; i < m_counter; i++)
+    {
+    	
+        Flight *myFlight = &m_flightArray[i];
+        
+        
+		myfile << myFlight->m_flightNumber<<endl;
+		myfile << myFlight->m_date<<" "<<myFlight->m_time<<endl;
+		myfile << myFlight->m_departVenue<<endl;
+		myfile << myFlight->m_arriveVenue<<endl;
+		myfile << myFlight->m_seats<<endl<<endl;
+		
+        
+    }
+	}
+    
+    myfile.close();
+}
 FlightDock::FlightDock(int numberOfFlight)
 {
-    m_flightArray = (Flight*) malloc(numberOfFlight * sizeof(Flight));
+    m_flightArray = new Flight[numberOfFlight];
     m_counter = 0;
     m_maxFlightCounter = 5;
 }
@@ -68,8 +183,16 @@ FlightDock::~FlightDock()
 {
     delete[] m_flightArray;
 }
+void FlightDock::getAll()
+{
+	for( int i = 0; i < m_counter; i++)
+    {
+        displayFlight(m_flightArray[i].m_flightNumber);
+        
+    }
+}
 
-bool FlightDock::findFlight(const string& flightNumber, Flight* myFlight)
+bool FlightDock::findFlight(const string& flightNumber)
 {
     const string flightNumberTrimed = trim(flightNumber);
     bool found = false;
@@ -79,21 +202,27 @@ bool FlightDock::findFlight(const string& flightNumber, Flight* myFlight)
         if(m_flightArray[i].m_flightNumber == flightNumber)
         {
             found = true;
-
-            if(myFlight != NULL)
-            {
-                myFlight = &m_flightArray[i];
-            }
-            break;
         }
     }
     return found;
 }
-// still thinkn to print information af a required flight
-void getFlightDetails(Flight* myFlight)
+
+bool FlightDock::findFlight(const string& flightNumber, int& myFlightIdx)
 {
-   	
+    const string flightNumberTrimed = trim(flightNumber);
+    bool found = false;
+
+    for( int i = 0; i < m_counter; i++)
+    {
+        if(m_flightArray[i].m_flightNumber == flightNumber)
+        {
+            found = true;
+            myFlightIdx = i;
+        }
+    }
+    return found;
 }
+
 
 void FlightDock::addFlight(string flightNumber, string date, string time, string departVenue, string arriveVenue, string seats)
 {
@@ -103,7 +232,7 @@ void FlightDock::addFlight(string flightNumber, string date, string time, string
         m_maxFlightCounter += 5;
     }
 
-    m_flightArray[m_counter].m_flightNumber   = trim(flightNumber);
+    m_flightArray[m_counter].m_flightNumber   = flightNumber;
     m_flightArray[m_counter].m_date           = trim(date);
     m_flightArray[m_counter].m_time           = trim(time);
     m_flightArray[m_counter].m_departVenue    = trim(departVenue);
@@ -111,18 +240,20 @@ void FlightDock::addFlight(string flightNumber, string date, string time, string
     m_flightArray[m_counter].m_seats          = trim(seats);
 
     m_counter++;
+    //displayFlight(flightNumber);
 }
 
 bool FlightDock::bookSeat(string flightNumber, string seat)
 {
     Flight * myFlight;
     bool booked = false;
-    if(!findFlight(flightNumber, myFlight))
+    int myFlightIdx;
+    if(!findFlight(flightNumber, myFlightIdx))
     {
-        cerr<<"invalid flight number"<<cout;
+        cout<<"invalid flight number"<<cout;
         return false;
     }
-
+    myFlight = &m_flightArray[myFlightIdx];
     stringstream iss(seat);
     int rowNumber;
     char seatClass;
@@ -207,9 +338,10 @@ bool FlightDock::bookSeat(string flightNumber, string seat)
                 tempSeatNumber.erase(found, found+1);
             }
         }
-
+		
         buff << bufferString << temRowNumber << " " << tempSeatClass << " " << tempSeatNumber << "\n";
         bufferString = buff.str();
+        
     }
 
     if(booked)
@@ -244,18 +376,20 @@ void removeDupWord(string str,string* date, string* time)
 	
 } 
 
-void collectData(FlightDock fd)
+void collectData(FlightDock& fd)
 {
-	string line;
   int lineNumber = 1;
   
+  
+  string line;
   string number;
   string date;
   string time;
   string deptair;
   string arriveair;
   string seats;
- 
+  
+
   ifstream myfile;
   myfile.open("Flights.txt");
   //read each line and store the variables. when empty line found start new.
@@ -263,7 +397,7 @@ void collectData(FlightDock fd)
   {
     while ( getline (myfile,line) )
     {
-    	cout<< line<<'\n';
+    	//cout<< line<<'\n';
     	if (lineNumber==1)
 		{
 			number = line;
@@ -297,11 +431,12 @@ void collectData(FlightDock fd)
 		{
 			lineNumber =1;
 			fd.addFlight(number,date,time,deptair,arriveair,seats);
-			
+			seats.clear();
 		}
 		
       
     }
+    
     myfile.close();
   }
 
@@ -322,20 +457,24 @@ void Menu(){
 
 int main()
 {
+	FlightDock fd(5);
+	collectData(fd);
+	
 	int command;
 	Menu();
 	cin>>command;
-	
-	FlightDock fd(5);
-	//collectData(fd);
-    
-    
-    
-    Flight* fl;
+	Flight* fl;
     string number;
+    string seatLoc;
+    string ccc;
+    int seatCount;
+    
 switch(command){
     case 1 :
-         cout << "Excellent!" << endl; 
+         cout << "Displaying all flights !" << endl;
+         
+         fd.getAll();
+         main();
          break;
     case 2 :
     	 cout << "View Flight! " << endl;
@@ -344,27 +483,58 @@ switch(command){
     	 if (fd.findFlight(number))
     	 {
     	 	cout << "Flight found! " << endl;
-    	 	getFlightDetails(number);
+    	 	fd.displayFlight(number);
 		 }
-		 else 
+		 else
 		 {
 			cout << "Flight not found! " << endl;
 		 }
-		 break; 
+		 main();
+		 break;
     case 3 :
-         
+    	cout << "Enter Flight number : " ;
+    	cin >> number;
+    	cout << "Enter number of seats : " ;
+    	 cin>>seatCount;
+		 if(fd.checkAvail(number,seatCount))
+		 {
+		 	cout<<"Seat Available"<<endl;
+		 }
+		 else
+		 {
+		 	cout<<"Seat Not Available"<<endl;
+		 }
+		 main();
          break;
     case 4 :
-         
+    	cout << "Enter Flight number : " ;
+    	cin>> number;
+    	
+    	cout << "Enter Seat location : " ;
+		getline(cin,seatLoc);
+		getline(cin,seatLoc);
+		cout<<endl;
+    	
+    	
+		//cin.getline(cin,seatLoc);
+		
+		if(fd.bookSeat(number,seatLoc))
+		{
+			cout << "Flight : "<<number <<" Seat : "<<seatLoc<<" Booked\n" ;
+		}
+		else 
+		{
+            cout << "Not booked"<<endl ;	
+		}
+		main();
          break;
     case 5 :
-         
+		 fd.writeFile();
+		 
          break;
 }
-
-  
-    return 0;
+return 0;
+    
 }
-
 
 
